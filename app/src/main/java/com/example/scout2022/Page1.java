ee00134f70313b6127e17b3a744ec38b298094a0
“@mannijor000” <mannijor000@usd232.org>
2022-01-22 14:45:03 -0600

    initial commit

---
+++ app/src/main/java/com/example/scout2022/Page1.java
@@ -0,0 +1,24 @@
+package com.example.scout2022;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * creates page
+ */
+public class Page1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page1, container, false);
+    }
+}
